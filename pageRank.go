4e9da183f26022cd031d77df05642587cab943ca
Quentin M. Kniep <quentin@travelwell.io>
2021-01-09 14:04:43 +0000

    even faster PageRank computation
    using sparse matrix vector multiplication from github.com/james-bowman/sparse

--- pageRank.go
+++ pageRank.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/james-bowman/sparse"
 	"gonum.org/v1/gonum/mat"
 	"math"
 	"sort"
 	"time"
 )
 
-const EPS = 1e-7
+const ConvergenceThreshold = 1e-7
 
-type webRank struct {
+type websiteRank struct {
 	url  string
 	rank float64
 }
@@ -38,9 +39,9 @@ func PageRank(links map[string][]string) {
 	var R = mat.NewVecDense(len(initialRanks), initialRanks)
 	var ONE = mat.NewVecDense(len(oneData), oneData)
 	iterations := 0
-	for delta > EPS {
+	for delta > ConvergenceThreshold {
 		oldR := mat.VecDenseCopyOf(R)
-		R.MulVec(A, oldR)
+		R = sparse.MulMatVec(false, 1.0, A, R, nil)
 		R.AddScaledVec(ONE, 0.85, R)
 		oldR.SubVec(oldR, R)
 		delta = mat.Norm(oldR, 1)
@@ -48,9 +49,9 @@ func PageRank(links map[string][]string) {
 	}
 
 	// print websites by rank
-	var pageRanks []webRank
+	var pageRanks []websiteRank
 	for url, id := range indexMap {
-		pageRanks = append(pageRanks, webRank{url: url, rank: initialRanks[id]})
+		pageRanks = append(pageRanks, websiteRank{url: url, rank: initialRanks[id]})
 	}
 	sort.Slice(pageRanks, func(i, j int) bool { return pageRanks[i].rank > pageRanks[j].rank })
 	for _, wr := range pageRanks[:3] {
@@ -86,14 +87,15 @@ func buildIndexMap(links map[string][]string) map[string]int {
 	return indexMap
 }
 
-func buildAdjMatrix(indexMap map[string]int, links map[string][]string) *mat.Dense {
-	var matrix = make([]float64, len(links)*len(links))
+func buildAdjMatrix(indexMap map[string]int, links map[string][]string) *sparse.CSR {
+	var matrix = sparse.NewDOK(len(links), len(links))
 	for website, outgoingLinks := range links {
 		websiteID := indexMap[website]
 		for _, outgoing := range outgoingLinks {
 			outgoingID := indexMap[outgoing]
-			matrix[outgoingID*len(links)+websiteID] += 1.0 / float64(len(outgoingLinks))
+			newValue := matrix.At(outgoingID, websiteID) + 1.0 / float64(len(outgoingLinks))
+			matrix.Set(outgoingID, websiteID, newValue)
 		}
 	}
-	return mat.NewDense(len(links), len(links), matrix)
+	return matrix.ToCSR()
 }
